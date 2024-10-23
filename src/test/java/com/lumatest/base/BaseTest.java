fbd734f4db00248b8f692d2964751f96e94dfa7b
iuliia-kovalenko <yuliakovalenko83@gmail.com>
2024-10-23 11:11:26 +0200

    Add tests and update build.yaml

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -3,12 +3,17 @@ package com.lumatest.base;
 import com.lumatest.utils.DriverUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
+
 public abstract class BaseTest {
     private WebDriver driver;
+    private WebDriverWait wait5;
 
     @BeforeSuite
     protected void setupWebDriverManager() {
@@ -28,8 +33,14 @@ public abstract class BaseTest {
         }
     }
 
-
     public WebDriver getDriver() {
         return this.driver;
+    }
+
+    public WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+        return wait5;
     }
 }
